46f9b282cbe19396bf8fb4412944c1c4e9b56089
jowczarek-securesoftwareai <jakub.owczarek@securesoftware.ai>
2025-10-01 16:03:14 +0200

    add a cluster setup script

---
+++ setup_cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install CUDA 12.6
+CUDA_URL=https://developer.download.nvidia.com/compute/cuda/12.6.3/local_installers/cuda_12.6.3_560.35.05_linux.run
+
+wget -q --show-progress --progress=bar:force:noscroll -O cuda_installer.run $CUDA_URL
+sudo sh cuda_installer.run --silent --toolkit --override
+
+export CUDA_HOME=/usr/local/cuda-12.6
+
+# Install PyTorch with CUDA 12.6
+PYTORCH_INDEX_URL=https://download.pytorch.org/whl/cu126
+
+pip3 install torch torchvision torchaudio --index-url $PYTORCH_INDEX_URL
+
+# Additional packages for building extensions
+pip3 install packaging ninja wheel setuptools setuptools-scm
+
+pip3 install flash-attn
+
+pip install -r requirements.txt
+
+wandb login
